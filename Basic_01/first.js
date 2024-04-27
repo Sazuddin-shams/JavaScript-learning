0efcf93a93b7426ab45cdc0f5e0f7d05421ac58c
Sazuddin-shams <164725956+Sazuddin-shams@users.noreply.github.com>
2024-04-27 15:03:08 +0000

    datatypes

--- Basic_01/first.js
+++ Basic_01/first.js
@@ -1,1 +1,14 @@
 console.log("Hellow Sazuddin how are you today");
+"use strict";
+// alert("Hellow"); //we are using nodejs not browser
+console.log(3+4);
+let age=18;
+//number->int 2 to the power of 52--- interger type
+//boolean-> true/false-- yes or no
+//bigint-> take very big value - integer
+//null-> Standalone value----object type
+//undefine-> undefine-
+
+console.log(typeof undefined);
+console.log(typeof null); //object type
+
